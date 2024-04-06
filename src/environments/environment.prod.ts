39a2cf3c65456cb9ed6926544491fd740645b7eb
yashraj <yashraj.s2711@gmail.com>
2024-04-06 22:53:09 +0530

    d

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseurl: 'http://13.233.159.68:3000/'
+  baseurl: 'https://calm-blazer-yak.cyclic.app/'
+  // baseurl: 'http://13.233.159.68:3000/'
 };
