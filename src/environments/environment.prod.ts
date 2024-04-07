ab794fe41067246ceed21c1b248e6445bc720623
yashraj <yashraj.s2711@gmail.com>
2024-04-07 11:34:00 +0530

    add secure baseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  baseurl: 'https://calm-blazer-yak.cyclic.app/'
-  // baseurl: 'http://13.233.159.68:3000/'
+  baseurl: 'https://hack.hackmebackend.publicvm.com/'
 };
